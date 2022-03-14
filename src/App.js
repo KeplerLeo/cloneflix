abeea9693c7ba1a240b8f136d94dcdd4e7a64dfc
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-03-14 14:15:29 -0300

    api tmdb complete and start the home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react"; 
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
 
 export default () => {
+
+  const [homeList, setHomeList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pega a lista completa
+      let list = await Tmdb.getHomeList();
+      setHomeList(list);
+    }
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="page">
+      <section className="lists">
+        {homeList.map((item, key)=>(
+        <div>
+          {item.title}
+        </div>
+        ))}
+      </section>
     </div>
-  );
-}
+  ); }
