cda104400a633cfef47c1a8b1bfaf3d6b2575490
Leonardo Kepler <leokepler@gmail.com>
2024-01-26 10:26:34 -0300

    retrieving requests from categories

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -3,7 +3,6 @@ const API_BASE = 'https://api.themoviedb.org/3';
 
 const MOVIE_ENDPOINT = (id) => `/movie/${id}?language=pt-BR&api_key=${API_KEY}`;
 const TV_ENDPOINT = (id) => `/tv/${id}?language=pt-BR&api_key=${API_KEY}`;
-const ORIGINALS_ENDPOINT = `/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`;
 
 const basicFetch = async (endpoint) => {
   const req = await fetch(`${API_BASE}${endpoint}`);
@@ -27,7 +26,42 @@ export default {
     await fetchCategory(
       'originals',
       'Originais do Netflix',
-      ORIGINALS_ENDPOINT,
+      `/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'trending',
+      'Recomendados para você',
+      `/trending/all/week?language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'toprated',
+      'Em alta',
+      `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'action',
+      'Ação',
+      `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'adventure',
+      'Aventura',
+      `/discover/movie?with_genres=12&language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'comedy',
+      'Comédia',
+      `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'horror',
+      'Terror',
+      `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`,
+    ),
+    await fetchCategory(
+      'documentary',
+      'Documentário',
+      `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`,
     ),
   ],
 
